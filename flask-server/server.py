d7bece9f030e3efb9327000f6247e2cb7eebb850
Prashansa-Singh <88768231+Prashansa-Singh@users.noreply.github.com>
2022-08-24 14:36:46 +1000

    Added POST request

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -12,5 +12,13 @@ def home():
 def recipes():
     return {"recipes" : ["Apple", "Banana", "Orange"]}
 
+# POST API
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['text']}
+     return jsonify(dictToReturn)
+
+# this needs to be below all APIs
 if __name__ == "__main__":
     app.run(port=9000, debug=True)
