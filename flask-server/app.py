3cfe66ce434f91d138d288bb62c79947d4debb2a
Prashansa-Singh <88768231+Prashansa-Singh@users.noreply.github.com>
2022-08-26 20:12:42 +1000

    Added removeOne() API

--- flask-server/app.py
+++ flask-server/app.py
@@ -10,8 +10,8 @@ def flask_mongodb_atlas():
     return "flask mongodb atlas!"
 
 
-# must use double quotes in postman for POST body
 # API to add one recipe to database
+# When testing, must use double quotes in postman for POST body
 @app.route('/addOne', methods=["POST"])
 def addOne():
      input_json = request.get_json(force=True) 
@@ -35,5 +35,19 @@ def getAll():
     return jsonify(data)
         
 
+# API to remove a recipe from the database using the recipe's name
+# e.g. body might be {"name": "Eggs"}
+@app.route('/removeOne', methods=["DELETE"])
+def removeOne():
+     input_json = request.get_json(force=True)      
+     dictToReturn = {'name': input_json['name']}
+     jsonify_version = jsonify(dictToReturn)     
+     
+     # remove from database
+     db.db.collection.delete_one(dictToReturn)
+     return jsonify_version
+        
+
+# Put this below all APIs
 if __name__ == '__main__':
     app.run(port=8000)
